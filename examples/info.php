da3ddaacf5f57033e9ab9ca321e77f0a3116b7d9
Hammed Oyedele <itz.harmid@gmail.com>
2025-11-26 10:15:09 +0100

    chore: remove unused `bringToFront` method call in info.php example

--- examples/info.php
+++ examples/info.php
@@ -6,8 +6,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 
 $app = new Application('2025');
 
-$app->bringToFront();
-
 echo "Name: ".$app->name.PHP_EOL;
 
 echo "Locale: ".$app->locale.PHP_EOL;
